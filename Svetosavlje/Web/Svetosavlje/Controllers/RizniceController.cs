04acc507f245851290e9d4e521ab0ef7cf32ce92
Dejan Bujak <dbujak@properit.com>
2014-01-24 11:08:57 -0500

    Added sections for Riznice
    Link on the navigation bar, controller, folders, index view

---
+++ Svetosavlje/Web/Svetosavlje/Controllers/RizniceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Svetosavlje.Controllers
+{
+    public class RizniceController : Controller
+    {
+        //
+        // GET: /Riznice/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
