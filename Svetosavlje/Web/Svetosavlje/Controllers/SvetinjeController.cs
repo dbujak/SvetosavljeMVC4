4bdbe42591bf3c63ebe5dc2b5e41edaf132d892c
Dejan Bujak <dbujak@properit.com>
2014-04-29 16:23:01 -0400

    Svetinje, zapoceto prebacivanje
    Napravio i povezao kontroler sa stranicom.

---
+++ Svetosavlje/Web/Svetosavlje/Controllers/SvetinjeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Svetosavlje.Controllers
+{
+    public class SvetinjeController : Controller
+    {
+        //
+        // GET: /Svetinje/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
