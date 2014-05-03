61052601da96285ce80c001092b92a5ae006c6b3
Vlad.A <vladimir.ajder@gmail.com>
2014-05-03 02:14:09 +0200

    Оквир за Молитве
    Оквир за странице Молитви је имплементиран. Слиједи имплементација
    читања садржаја из базе. Затим упис и манипулација.

---
+++ Svetosavlje/Web/Svetosavlje/Controllers/MolitveController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Svetosavlje.Controllers
+{
+    public class MolitveController : Controller
+    {
+        //
+        // GET: /Molitve/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrikazMolitve()
+        {
+            return View();
+        }
+
+    }
+}
